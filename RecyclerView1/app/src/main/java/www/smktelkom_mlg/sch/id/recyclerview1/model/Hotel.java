a40af5370619d899dd6399421fc741083af95e83
FirmansyahD <dewantaraf855@gmail.com>
2019-03-13 20:59:23 +0700

    selesai pak

---
+++ RecyclerView1/app/src/main/java/www/smktelkom_mlg/sch/id/recyclerview1/model/Hotel.java
@@ -0,0 +1,13 @@
+package www.smktelkom_mlg.sch.id.recyclerview1.model;
+
+import android.graphics.drawable.Drawable;
+
+public class Hotel {
+    public String judul, deskripsi;
+    public Drawable foto;
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
